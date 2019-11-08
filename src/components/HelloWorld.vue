31cec77d57c6396b11db6177c57d52ddd97fce57
13162305067@163.com <13162305067@163.com>
2019-11-08 15:31:17 +0800

    vue中使用谷歌地图vue2-google-maps,显示谷歌地图，（项目开启后需要vpn和谷歌地图的key支持才可显示地图）

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+
+<template>
+    <div>
+        <!-- <label>
+            AutoComplete
+            <GmapAutocomplete :position.sync="markers[0].position" @keyup.enter="usePlace" @place_changed="setPlace">
+            </GmapAutocomplete>
+            <button @click="usePlace">Add</button>
+        </label> -->
+
+
+        <gmap-map
+            :center="centers"
+            :zoom="11"
+            map-type-id="terrain"
+            style="width: 100%; height: 340px"
+        >
+            <gmap-marker
+                @dragend="updateMaker"
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="centers=m.position"
+            ></gmap-marker>
+              <!-- @click="centers=m.position" -->
+        </gmap-map>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                centers: {lat: 39.90419989999999,lng: 116.4073963},
+                markers: [{
+                    position: {lat: 39.90419989999999,lng: 116.4073963}
+                }],
+                place: null,
+            }
+        },
+        description: 'Autocomplete Example (#164)',
+        mounted() {
+
+        },
+        methods: {
+            setPlace(place) {
+                this.place = place
+            },
+            setDescription(description) {
+                this.description = description;
+            },
+            usePlace(place) {
+                if (this.place) {
+                    var newPostion = {
+                        lat: this.place.geometry.location.lat(),
+                        lng: this.place.geometry.location.lng(),
+                    };
+                    this.center = newPostion;
+                    this.markers[0].position =  newPostion;
+                    this.place = null;
+                }
+            },
+            updateMaker: function(event) {
+                console.log('updateMaker, ', event.latLng.lat());
+                this.markers[0].position = {
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng(),
+                }
+            },
+        }
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: '您的谷歌地图的key',//申请的谷歌地图的key
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then disable the following:
+  // installComponents: true,
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
